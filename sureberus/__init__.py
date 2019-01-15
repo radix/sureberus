7aa3b905f44d32920e4301c47804d84cc71e90dc
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2019-01-15 14:46:30 -0600

    Handle defaults and add more tests

--- sureberus/__init__.py
+++ sureberus/__init__.py
@@ -41,17 +41,17 @@ def _normalize_dict(dict_schema, value, ctx):
         else:
             raise E.UnknownFields(value, extra_keys, stack=ctx.stack)
     for key, key_schema in dict_schema.items():
-        if key not in value:
-            replacement = _get_default(key, key_schema, value, ctx)
+        original_key = key
+        if 'rename' in key_schema:
+            key = key_schema.get('rename')
+        if original_key not in value:
+            replacement = _get_default(original_key, key_schema, value, ctx)
             if replacement is not _marker:
                 new_dict[key] = replacement
             elif key_schema.get('required', False) == True:
                 raise E.DictFieldNotFound(key, value=value, stack=ctx.stack)
-        if key in value:
-            original_key = key
-            if 'rename' in key_schema:
-                key = key_schema.get('rename')
-            new_dict[key] = _normalize_schema(key_schema, 
+        if original_key in value:
+            new_dict[key] = _normalize_schema(key_schema,
                 value[original_key], ctx.push_stack(original_key))
             excludes = key_schema.get('excludes', [])
             if not isinstance(excludes, list):
--- test_sure.py
+++ test_sure.py
@@ -218,6 +218,31 @@ def test_rename_with_coerce():
     val = {'foo': 2}
     assert normalize_schema(schema, val) == {'moo': '2'}
 
+def test_rename_with_default():
+    schema = S.Dict(schema={
+        'foo': {'rename': 'moo', 'type': 'boolean', 'default': True},
+    })
+    val = {}
+    assert normalize_schema(schema, val) == {'moo': True}
+
+    val = {'foo': False}
+    assert normalize_schema(schema, val) == {'moo': False}
+
+def test_rename_with_both_attributes_present():
+    schema = S.Dict(schema={
+        'foo': {'rename': 'moo', 'coerce': str},
+    }, allow_unknown=True)
+    val = {'foo': 1, 'moo': 2}
+    assert normalize_schema(schema, val) == {'moo': '1'}
+
+    # Yes, we can swap attributes
+    schema = S.Dict(schema={
+        'foo': {'rename': 'moo', 'coerce': str},
+        'moo': {'rename': 'foo', 'coerce': str},
+    }, allow_unknown=True)
+    val = {'foo': 1, 'moo': 2}
+    assert normalize_schema(schema, val) == {'moo': '1', 'foo': '2'}
+
 def test_rename_with_maxlength():
     schema = S.Dict(schema={
         'foo': {'rename': 'moo', 'maxlength': 3},

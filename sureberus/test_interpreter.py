e70ab1f2c7d6db31c5ceb07fd2c1862caf388408
Christopher Armstrong <227068+radix@users.noreply.github.com>
2019-06-28 10:39:38 -0600

    fix test_interpreter

--- sureberus/test_interpreter.py
+++ sureberus/test_interpreter.py
@@ -1,12 +1,12 @@
 import pytest
 
 from .interpreter import interpret
-from .instructions import CheckType
+from .instructions import CheckType, Transformer
 from . import INIT_CONTEXT
 from . import errors as E
 
 def test_check_type():
-    ins = [CheckType("integer")]
+    ins = Transformer([CheckType("integer")])
     assert interpret(ins, 3, INIT_CONTEXT) == 3
     with pytest.raises(E.BadType):
         interpret(ins, "foo", INIT_CONTEXT)

a5e61c1c4e9f6b09c93e3d8357d075f297f34f47
Christopher Armstrong <227068+radix@users.noreply.github.com>
2019-07-19 16:07:54 -0500

    bump to 0.9.1 for this bugfix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="sureberus",
-    version="0.9.0",
+    version="0.9.1",
     description="Validation & transformation.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",

7bae11c475d8814c413f12f5fa01285266599e48
Christopher Armstrong <227068+radix@users.noreply.github.com>
2019-07-29 16:26:28 -0500

    bump to 0.12.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="sureberus",
-    version="0.11.2",
+    version="0.12.0",
     description="Validation & transformation.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",

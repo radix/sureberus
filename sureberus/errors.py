77f66c0be91fce0b70fe58af1ef3d6fa18025c6f
Christopher Armstrong <227068+radix@users.noreply.github.com>
2023-02-07 11:14:19 -0600

    add support for pickling errors even when schemas or values are unpickleable
    This is a quick n dirty hack to force serialization of errors as strings.

--- sureberus/errors.py
+++ sureberus/errors.py
@@ -16,6 +16,20 @@ class SimpleSchemaError(SchemaError):
 
 
 class SureError(Exception):
+
+    def __reduce__(self):
+        # This is a workaround for a super obscure problem that make errors unpickleable
+        # in certain situations, when value, or parts of the schema, are unpickleable.
+        #
+        # First of all, why do we care about pickling exceptions? Because the python
+        # unittest code wants to pickle and unpickle exceptions when tests are run in
+        # parallel.
+        #
+        # So this is just a quick'n'dirty workaround to just serialize SureErrors as
+        # plain Exceptions with string messages. Printing an exception will be
+        # identical, but you will lose rich type info & individual fields.
+        return Exception, (str(self),)
+
     def __str__(self):
         stack = "root"
         stack += "".join("[{!r}]".format(el) for el in self.stack)
@@ -216,3 +230,20 @@ class RegisteredFunctionNotFound(SureError):
     setter = attr.ib()
     registry_name = attr.ib()
     stack = attr.ib()
+
+
+if __name__ == '__main__':
+    f = open("/tmp/blah", "w")
+    from sureberus import normalize_schema
+    import pickle
+    try:
+        normalize_schema({"type": "integer"}, f)
+    except Exception as e:
+        error = e
+    v = pickle.dumps(error)
+    print(v)
+    newv = pickle.loads(v)
+    print("OLD ERROR:")
+    print(error)
+    print("NEW ERROR:")
+    print(newv)
